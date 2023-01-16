4ae62b44ebe112c284ff4f7c5b2e3059db802b24
Rob C Morris <99232975+robcmorris@users.noreply.github.com>
2023-01-15 23:32:36 -0500

    Added some home and about sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,30 +4,138 @@
 	<title>Home</title>
 </svelte:head>
 
-<div class="relative h-screen w-full flex items-center justify-center text-center bg-cover bg-center" style="background-image:url(https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80);">
-  <div class="absolute top-0 right-0 bottom-0 left-0 bg-gray-900 opacity-75"></div>
-  
-  <main class="px-4 sm:px-6 lg:px-8 z-10">
-        <div class="text-center">
-          <h2 class="text-4xl tracking-tight leading-10 font-medium sm:text-5xl text-white sm:leading-none md:text-6xl">
-            <span class="text-indigo-600 font-bold">Hello, World!</span> I'm Rob!
-          </h2>
-          <p class="mt-3 text-white sm:mt-5 sm:text-md sm:max-w-xl sm:mx-auto md:mt-5">
-            An IT Guy learning Software Development.
-          </p>
-          <div class="mt-5 sm:mt-8 sm:flex justify-center">
-            <div class="rounded-md shadow">
-              <a href="/projects" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-regular rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:px-10">
-                Projects
-              </a>
-            </div>
-            <div class="mt-3 sm:mt-0 sm:ml-3">
-              <a href="/contact" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-regular rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:px-10">
-                Contact Me
-              </a>
-            </div>
-          </div>
-        </div>
-      </main>
-  
+<div
+	class="relative h-screen w-full flex items-center justify-center text-center bg-cover bg-center"
+	style="background-image:url(https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80);">
+	<div class="absolute top-0 right-0 bottom-0 left-0 bg-gray-900 opacity-75" />
+
+	<main class="px-4 sm:px-6 lg:px-8 z-10">
+		<div class="text-center">
+			<h2
+				class="text-4xl tracking-tight leading-10 font-medium sm:text-5xl text-white sm:leading-none md:text-6xl"
+			>
+				<span class="text-indigo-600 font-bold">Hello, World!</span> I'm Rob!
+			</h2>
+			<p class="mt-3 text-white sm:mt-5 sm:text-md sm:max-w-xl sm:mx-auto md:mt-5">
+				An IT Guy learning Software Development.
+			</p>
+			<div class="mt-5 sm:mt-8 sm:flex justify-center">
+				<div class="rounded-md shadow">
+					<a
+						href="/projects"
+						class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-regular rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:px-10"
+					>
+						Projects
+					</a>
+				</div>
+				<div class="mt-3 sm:mt-0 sm:ml-3">
+					<a
+						href="/contact"
+						class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-regular rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:px-10"
+					>
+						Contact Me
+					</a>
+				</div>
+			</div>
+		</div>
+	</main>
 </div>
+
+<!-- INTRO -->
+<section class="text-gray-700 body-font">
+  <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">About
+        <br class="hidden lg:inline-block">Rob Morris
+      </h1>
+      <p class="mb-8 leading-relaxed">I’m a CompTIA Secure Infrastructure Specialist from Jacksonville, FL. I’m skilled in installing, configuring, and maintaining computer systems & networking equipment. Actively pursing Software Development.</p>
+      <div class="flex justify-center">
+        <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Learn More</button>
+      </div>
+    </div>
+    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      <img class="object-cover object-center rounded" alt="hero" src="http://www.robcmorris.com/wp-content/uploads/2022/12/profile-pic.png">
+    </div>
+  </div>
+</section>
+
+<!-- CERTS BREIF-->
+<section class="text-gray-700 body-font border-t border-gray-200 text-center">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">My Certifications</h1>
+      <p class="lg:w-1/2 w-full leading-relaxed text-base">Some Certifications I've earned.</p>
+    </div>
+    <div class="flex flex-wrap -m-4">
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-300 p-6 rounded-lg">
+          <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+              <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+            </svg>
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2">CompTIA</h2>
+          <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+        </div>
+      </div>
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-300 p-6 rounded-lg">
+          <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+              <circle cx="6" cy="6" r="3"></circle>
+              <circle cx="6" cy="18" r="3"></circle>
+              <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+            </svg>
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Microsoft</h2>
+          <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+        </div>
+      </div>
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-300 p-6 rounded-lg">
+          <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+              <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg>
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2">AWS</h2>
+          <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+        </div>
+      </div>
+    </div>
+    <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Learn More</button>
+  </div>
+</section>
+
+<!-- QUOTE -->
+<section class="text-gray-700 body-font border-t border-gray-200">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-gray-400 mb-8" viewBox="0 0 975.036 975.036">
+        <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+      </svg>
+      <p class="leading-relaxed text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi placeat praesentium quidem aperiam, fugit aut inventore illo eaque omnis veniam hic cumque tempora corporis quia ullam delectus, totam doloremque voluptatum.</p>
+      <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span>
+      <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">John Doe</h2>
+      <p class="text-gray-500">IT Manager</p>
+    </div>
+  </div>
+</section>
+
+<!-- MAP -->
+<section class="text-gray-700 body-font relative">
+  <div class="absolute inset-0 bg-gray-300">
+    <iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=jacksonville%20%FL&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" style="filter: grayscale(1) contrast(1.2) opacity(0.4);"></iframe>
+  </div>
+  <div class="container px-5 py-24 mx-auto flex">
+    <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10">
+      <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Contact Me</h2>
+      <p class="leading-relaxed mb-5 text-gray-600">Feel free to reach out to me about projects.</p>
+      <input class="bg-white rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" placeholder="Email" type="email">
+      <textarea class="bg-white rounded border border-gray-400 focus:outline-none h-32 focus:border-indigo-500 text-base px-4 py-2 mb-4 resize-none" placeholder="Message"></textarea>
+      <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Submit</button>
+      <p class="text-xs text-gray-500 mt-3">Your information will not be shared, sold, or used for spam.</p>
+    </div>
+  </div>
+</section>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,6 +4,103 @@
     <title>About</title> 
 </svelte:head>
 
-<div class="py-32 text-center h-96">
-    <h2 class="font-extrabold text-4xl">About</h2>
-</div>
+<!-- INTRO -->
+<section class="text-gray-700 body-font">
+  <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">About Me
+        <br class="hidden lg:inline-block">Rob Morris
+      </h1>
+      <p class="mb-8 leading-relaxed">I developed a love for technology at a young age, starting with handheld electronics and video games. Over the years, I’ve gained a lot of experience with computer hardware, software, and programming. I eventually ended up with working in the IT industry at a local managed IT service provider.</p>
+      <p class="mb-8 leading-relaxed">When I’m not working, I’m an advocate for open-source software and enjoy self-hosting useful projects in my home lab. I like experimenting with Docker, Linux, and learning to program in various languages. I still play the occasional video games from time to time, but most of my free time is spent learning about new technologies and working towards new certifications.</p>
+      <div class="flex justify-center">
+        <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Learn More</button>
+      </div>
+    </div>
+    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      <img class="object-cover object-center rounded" alt="hero" src="http://www.robcmorris.com/wp-content/uploads/2022/12/profile-pic.png">
+    </div>
+  </div>
+</section>
+  
+<!-- CERTS -->
+<section class="text-gray-700 body-font border-t border-gray-200">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">My Certifications</h1>
+      <p class="lg:w-1/2 w-full leading-relaxed text-base">Some Certifications I've earned.</p>
+    </div>
+    <div class="flex flex-wrap -m-4">
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-300 p-6 rounded-lg">
+          <div class="w-24 h-24 inline-flex items-center justify-center rounded-full bg-gray-100 text-indigo-500 mb-4">
+            <img src="https://comptiawebsite.blob.core.windows.net/webcontent/images/default-source/siteicons/logoaplus.svg" alt="CompTIA A+ Certification Logo">
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2">CompTIA A+</h2>
+          <p class="leading-relaxed text-base">CompTIA A+ is an industry recognized credential with performance testing emphasizing the technologies and skills IT pros need to support a hybrid workforce.</p>
+        </div>
+      </div>
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-300 p-6 rounded-lg">
+          <div class="w-24 h-24 inline-flex items-center justify-center rounded-full bg-gray-100 text-indigo-500 mb-4">
+            <img src="https://comptiawebsite.blob.core.windows.net/webcontent/images/default-source/siteicons/logonetworkplus.svg" alt="CompTIA Network+ Certification Logo">
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2">CompTIA Network +</h2>
+          <p class="leading-relaxed text-base">CompTIA Network+ validates the technical skills needed to securely establish, maintain and troubleshoot the essential networks that businesses rely on.</p>
+        </div>
+      </div>
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-300 p-6 rounded-lg">
+          <div class="w-24 h-24 inline-flex items-center justify-center rounded-full bg-gray-100 text-indigo-500 mb-4">
+            <img src="https://comptiawebsite.blob.core.windows.net/webcontent/images/default-source/siteicons/logosecurityplus.svg" alt="CompTIA Security+ Certification Logo">
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2">CompTIA Security+</h2>
+          <p class="leading-relaxed text-base">CompTIA Security+ is a global certification that validates the baseline skills necessary to perform core security functions. </p>
+        </div>
+      </div>
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-300 p-6 rounded-lg">
+          <div class="w-24 h-24 inline-flex items-center justify-center rounded-full bg-gray-100 text-indigo-500 mb-4">
+            <img src="https://learn.microsoft.com/en-us/media/learn/certification/badges/microsoft-certified-fundamentals-badge.svg" alt="Micosoft 365 Fundamentals Certification Logo">
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Microsoft 365 Fundamentals</h2>
+          <p class="leading-relaxed text-base">Microsoft 365 Fundamentals provides knowledge on adopting cloud services, the basics of a Software as a Service (SaaS) cloud model, and the value of implementing Microsoft 365 cloud services.</p>
+        </div>
+      </div>
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-300 p-6 rounded-lg">
+          <div class="w-24 h-24 inline-flex items-center justify-center rounded-full bg-gray-100 text-indigo-500 mb-4">
+            <img src="https://learn.microsoft.com/en-us/media/learn/certification/badges/microsoft-certified-fundamentals-badge.svg" alt="Micosoft 365 Fundamentals Certification Logo">
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Microsoft Azure Fundamentals</h2>
+          <p class="leading-relaxed text-base">Microsoft Azure Fundamentals demonstrates a fundamental knowledge of cloud concepts, along with Azure services, workloads, security, privacy, pricing, and support.</p>
+        </div>
+      </div>
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-300 p-6 rounded-lg">
+          <div class="w-24 h-24 inline-flex items-center justify-center rounded-full bg-gray-100 text-indigo-500 mb-4">
+            <img src="https://d1.awsstatic.com/training-and-certification/certification-badges/AWS-Certified-Cloud-Practitioner_badge.634f8a21af2e0e956ed8905a72366146ba22b74c.png" alt="AWS Certified Cloud Practitioner Certification Logo">
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2">AWS Cloud Certifited Practioner</h2>
+          <p class="leading-relaxed text-base">Knowledge of core AWS services and use cases, billing and pricing models, security concepts, and how cloud impacts your business.</p>
+        </div>
+      </div>
+    </div>
+    <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Learn More</button>
+  </div>
+</section>
+  
+<!-- QUOTE -->
+<section class="text-gray-700 body-font border-t border-gray-200">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-gray-400 mb-8" viewBox="0 0 975.036 975.036">
+        <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+      </svg>
+      <p class="leading-relaxed text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi placeat praesentium quidem aperiam, fugit aut inventore illo eaque omnis veniam hic cumque tempora corporis quia ullam delectus, totam doloremque voluptatum.</p>
+      <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span>
+      <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">John Doe</h2>
+      <p class="text-gray-500">IT Manager</p>
+    </div>
+  </div>
+</section>
